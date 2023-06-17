7b400481d47a4f1aa8848ad779a93081d832c459
TylerDurden-cmd <125083855+TylerDurden-cmd@users.noreply.github.com>
2023-06-18 00:56:13 +0200

    mongo nog asynchroon maken

---
+++ index.ts
@@ -0,0 +1,43 @@
+import express, { urlencoded } from "express"
+import { MongoClient } from "mongodb";
+
+const uri = "mongodb+srv://dbUserJo:r71D2n5lRpc4PZMG@db-adjo.zpq4sxl.mongodb.net/WebOntwikkeling?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+const app = express();
+
+app.set("view engine","ejs");
+app.set("port",3000);
+app.use(express.static("Public"));
+
+app.use(express.urlencoded())
+
+
+app.get("/",(req,res)=>{
+    res.render('index')
+})
+
+app.post("/",(req,res)=>{
+    try{
+        client.connect();
+
+        const db = client.db("Portfolio").collection("Comments");
+    let naam = req.body.naam;
+    let Email = req.body.email;
+    let comment = req.body.comment;
+
+    db.insertOne({naam,Email,comment});
+}catch(e){
+    console.error(e);
+}finally{
+    client.close()
+    console.log("check the db")
+}
+res.render("index")
+
+
+})
+
+app.listen(app.get("port"),()=>{
+    console.log("{Portfolio Running} http://localhost:" + app.get("port"));
+})
